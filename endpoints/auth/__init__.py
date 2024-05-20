11d21b20b7e94f92be64ae2bc665aace59428aa3
Adeniyi Olanrewaju <100721103+engrmarkk@users.noreply.github.com>
2024-05-20 11:53:05 +0100

    Update __init__.py

--- endpoints/auth/__init__.py
+++ endpoints/auth/__init__.py
@@ -1,7 +1,6 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from models import User, Task
 from extensions import db
-from flask_jwt_extended import current_user
 from utils import return_access_token
 
 auth_blp = Blueprint("auth", __name__)
@@ -15,26 +14,26 @@ def register():
         password = data.get("password")
 
         if not username:
-            return {"message": "No username provided"}, 400
+            return jsonify({"message": "No username provided"}), 400
         if not password:
             return {"message": "No password provided"}, 400
 
         if User.username_exist(username):
-            return {"message": "Username already exist"}, 400
+            return jsonify({"message": "Username already exist"}), 400
 
         validation_message = User.password_validation(password)
 
         if validation_message:
-            return {"message": validation_message}, 400
+            return jsonify({"message": validation_message}), 400
         hashed_password = User.hash_password(password)
         user = User(username=username.lower(), password=hashed_password)
         db.session.add(user)
         db.session.commit()
-        return {"message": "User created successfully"}, 201
+        return jsonify({"message": "User created successfully"}), 201
     except Exception as e:
         print(e, "error@Register")
         db.session.rollback()
-        return {"message": "Network Error"}, 500
+        return jsonify({"message": "Network Error"}), 500
 
 
 
@@ -48,25 +47,25 @@ def login():
         password = data.get("password")
 
         if not username:
-            return {"message": "Username is required"}, 400
+            return jsonify({"message": "Username is required"}), 400
 
         if not password:
-            return {"message": "Password is required"}, 400
+            return jsonify({"message": "Password is required"}), 400
 
         user = User.username_exist(username)
 
         if not user:
-            return {"message": "User does not exist"}, 400
+            return jsonify({"message": "User does not exist"}), 400
 
         if not User.verify_password(password, user.password):
-            return {"message": "Incorrect password"}, 400
+            return jsonify({"message": "Incorrect password"}), 400
 
         access_token = return_access_token(user)
 
         if not access_token:
-            return {"message": "Something went wrong"}, 500
+            return jsonify({"message": "Something went wrong"}), 500
 
-        return {"message": "Login successful", "access_token": access_token}, 200
+        return jsonify({"message": "Login successful", "access_token": access_token}), 200
     except Exception as e:
         print(e, "error@Login")
-        return {"message": "Network Error"}, 500
+        return jsonify({"message": "Network Error"}), 500
